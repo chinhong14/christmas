2f6cca2afde8b34876ec484317af1ac4f4e595a5
chinhong141 <23005149@siswa.um.edu.my>
2025-12-17 20:06:41 +0800

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 1. THIS IS THE NEW LINE YOU NEED
+      base: "/christmas/",
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -21,3 +24,27 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
+// import path from 'path';
+// import { defineConfig, loadEnv } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, '.', '');
+//     return {
+//       server: {
+//         port: 3000,
+//         host: '0.0.0.0',
+//       },
+//       plugins: [react()],
+//       define: {
+//         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+//         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+//       },
+//       resolve: {
+//         alias: {
+//           '@': path.resolve(__dirname, '.'),
+//         }
+//       }
+//     };
+// });
